914f02d0160fb6aa1f21404b7c51e19a9c695e37
Hyundoo, Jin <gusen0927@gmail.com>
2018-12-06 00:12:08 +0900

    embedding with seq2seq

--- code/make_embedding.py
+++ code/make_embedding.py
@@ -4,14 +4,15 @@ import argparse
 import utils
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-l', '--language', help = 'select language from eng, e, kor, or k')
 parser.add_argument('-m', '--model', help = 'select embedding model from fasttext, f, word2vec, w, elmo, or e')
-parser.add_argument('-d', '--dataset', help = 'select dataset from \n [ENG] \n twitter, t, sms, s, keyboard, k \n [KOR] \n movie, m, kin, k')
+parser.add_argument('-d', '--dataset', help = 'select dataset from w(wikinews), p(wordprediction)')
 parser.add_argument('-s', '--size', help = 'embedding size', default = 300)
 parser.add_argument('-e', '--epochs', help = 'total epochs', default = 10)
 parser.add_argument('-r', '--learning_rate', help = 'learning_rate for training', default = 0.025)
 
-arguments = parser.parse_args()
-embed_model = utils.Embedding_Model(arguments)
-embed_model.train_model()
-embed_model.save_model()
+
+if __name__ == '__main__' :
+    arguments = parser.parse_args()
+    embed_model = utils.Embedding_Model(arguments)
+    embed_model.train_model()
+    embed_model.save_model()
--- code/utils.py
+++ code/utils.py
@@ -3,26 +3,19 @@ from gensim.models import Word2Vec, FastText
 import pickle
 import time
 import psutil
+import glob
 
 class Embedding_Model() :
     def __init__(self, arguments) :
         self.argument_setting(arguments)
-        self.version = '{}_{}_{}'.format(self.language, self.model, self.dataset)
+        self.version = '{}_{}'.format(self.model, self.dataset)
         self.model_path = os.path.join('../model', self.dataset)
-        self.dataset_path = os.path.join('../data', self.language, self.dataset)
+        self.dataset_path = os.path.join('../data', self.dataset)
         self.size = arguments.size
         self.epochs = arguments.epochs
         self.learning_rate = arguments.learning_rate
 
     def argument_setting(self, arguments) :
-        # set language
-        if arguments.language == 'kor' or arguments.language == 'k' :
-            self.language = 'kor'
-        elif arguments.language == 'eng' or arguments.language == 'e' :
-            self.language = 'eng'
-        else :
-            raise AttributeError('Undefined language.')
-        
         # set model
         if arguments.model == 'f' or arguments.model == 'fasttext' :
             self.model = 'fasttext'
@@ -34,36 +27,26 @@ class Embedding_Model() :
             raise AttributeError('Undefined model.')
         
         # set dataset
-        if self.language == 'eng' :
-            if arguments.dataset == 'twitter' or arguments.dataset == 't' :
-                self.dataset = 'twitter'
-            elif arguments.dataset == 'sms' or arguments.dataset == 's' :
-                self.dataset = 'sms'
-            elif arguments.datset == 'keyboard' or arguments.dataset == 'k' :
-                self.dataset = 'keyboard'
-            else :
-                raise AttributeError('Undefined dataset.')
-        
-        elif self.language == 'kor' :
-            if arguments.dataset == 'movie' or arguments.dataset == 'm' :
-                self.dataset = 'movie'
-            elif arguments.dataset == 'kin' or arguments.dataset == 'k' :
-                self.dataset = 'kin'
-            else :
-                raise AttributeError('Undefined datset.')
+        if arguments.dataset == 'wikinews' or arguments.dataset == 'w' :
+            self.dataset = 'wikinews'
+        elif arguments.dataset == 'wordprediction' or arguments.dataset == 'p' :
+            self.dataset = 'wordprediction'
         else :
-            raise AttributeError('Undefined language.')
-        print('Arguments seted. \n model : {} \n language : {} \n dataset : {}'.format(self.model, self.language, self.dataset))
+            raise AttributeError('Undefined dataset.')
+        
+        print('Arguments seted. \n model : {} \n dataset : {}'.format(self.model, self.dataset))
 
     def get_version(self) :
         return self.version
 
     def train_model(self) :
         start_time = time.time()
-        with open(self.dataset_path + '.pickle', 'rb') as f :
-            temp_data = pickle.load(f)
+        files = glob.glob(self.dataset_path + '/*.txt')
+        temp_data = list()
+        for file in files :
+            with open(file, 'r') as f :
+                temp_data.extend(f.readlines())
         print('data_loaded. Elapsed {} seconds'.format(round(time.time() - start_time, 3)))
-
         start_time = time.time()
         if self.model == 'fasttext' :
             self.model = FastText(list(map(lambda x : x.split(), temp_data)), size = self.size, iter = self.epochs, alpha = self.learning_rate, workers = psutil.cpu_count())
@@ -77,6 +60,6 @@ class Embedding_Model() :
     def save_model(self, file_path = None) :
         if file_path is None :
             file_path = self.model_path
-        model_path = os.path.join(file_path, self.version + '.model')        
+        model_path = os.path.join(file_path, self.version + '.model') 
         self.model.save(model_path)
         print('Model saved. path : {}'.format(model_path))
